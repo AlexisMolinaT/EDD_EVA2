0b4240d2a6889884a1315a9fd56b747db45a438a
AlexisMolinaT <104458053+AlexisMolinaT@users.noreply.github.com>
2022-09-29 09:57:05 -0600

    Add files via upload

--- EVA2_1_LISTA_SIMPLE/src/eva2_1_lista_simple/EVA2_1_LISTA_SIMPLE.java
+++ EVA2_1_LISTA_SIMPLE/src/eva2_1_lista_simple/EVA2_1_LISTA_SIMPLE.java
@@ -12,14 +12,24 @@ public class EVA2_1_LISTA_SIMPLE {
     public static void main(String[] args) {
         // TODO code application logic here
         Lista miLista = new Lista();
-        miLista.agreagar(10);
+        ///int[] arreglo = new int [1000000];
+        
+        for (int i = 0; i < 1000000; i++) { //(N^2)
+            miLista.agreagar(1000);
+            
+        }
+        System.out.println("Conteo de nodos: " + miLista.tamaLista());
+        
+        
+        
+        /*miLista.agreagar(10);
         miLista.agreagar(20);
         miLista.agreagar(30);
         miLista.agreagar(40);
         miLista.agreagar(50);
         miLista.agreagar(60);
         miLista.imprimir();
-        
+        */
                 
     }
     
--- EVA2_1_LISTA_SIMPLE/src/eva2_1_lista_simple/Lista.java
+++ EVA2_1_LISTA_SIMPLE/src/eva2_1_lista_simple/Lista.java
@@ -5,44 +5,69 @@ package eva2_1_lista_simple;
  * @author Alexis M.
  */
 public class Lista {
+
     private Nodo inicio;
+    private Nodo fin;
 
     //POR DEFAULT, LA LISTA ESRA VACIA
     public Lista() {
         this.inicio = null;  //NO HAY NODOS EN LA LISTA
+        this.fin = null;
     }
-    
-    
-    public void imprimir(){
+
+    public void imprimir() {
         Nodo temp = inicio;
-            //¿COMO MUEVO A TEMP?
-            while(temp.getSiguiente() != null){ //EL WHILE SIRVE PARA MOVERNOS  
-                System.out.print(temp.getValor() + " - ");
-                temp = temp.getSiguiente();
-                
-            }
+        //¿COMO MUEVO A TEMP?
+        while (temp != null) { //EL WHILE SIRVE PARA MOVERNOS  
+            System.out.print(temp.getValor() + " - ");
+            temp = temp.getSiguiente();
+
+        }
     }
-    
-    
+
     //AGREGAR UN NODO AL FINAL DE LA LISTA
-    public void agreagar(int valor){
+    //SOLUCION 1: O(N)
+    public void agreagar(int valor) {
         Nodo nuevoNodo = new Nodo(valor);
-        
+
         //VERIFICAR SI HAY NODOS EN LA LISTA
-        if(inicio == null){//NO HAY NODOS EN LA LISTA
+        if (inicio == null) {//NO HAY NODOS EN LA LISTA
             inicio = nuevoNodo;
-    }else{ //HAY NODOS EN LA LISTA
-        //HAY QUE MOVERNOS POR LA LISTA
-        //USANDO UN NODO TEMPORAL HASTA EL
-        //ULTIMO NODO DE LA LISTA
-            Nodo temp = inicio;
-            //¿COMO MUEVO A TEMP?
-            while(temp.getSiguiente() != null){ //EL WHILE SIRVE PARA MOVERNOS  
-                temp = temp.getSiguiente();
-                
-            }
-            temp.setSiguiente(nuevoNodo);
+            fin = nuevoNodo;
+        } else { //HAY NODOS EN LA LISTA
+            //HAY QUE MOVERNOS POR LA LISTA
+            //USANDO UN NODO TEMPORAL HASTA EL
+            //ULTIMO NODO DE LA LISTA
+            /*Nodo temp = inicio;
+             //¿COMO MUEVO A TEMP?
+             while (temp.getSiguiente() != null) { //EL WHILE SIRVE PARA MOVERNOS  
+             temp = temp.getSiguiente();
+
+             }
+             temp.setSiguiente(nuevoNodo);*/
+            fin.setSiguiente(nuevoNodo);
+            fin = nuevoNodo;
+        }
     }
-    
-}
+
+    public int tamaLista() {
+        int cont = 0;
+        Nodo temp = inicio;
+        //¿COMO MUEVO A TEMP?
+        while (temp != null) {
+            cont++;
+            temp = temp.getSiguiente();
+
+        }
+        return cont;
+    }
+
+    public void agregarEn(int valor, int pos) {
+            //¿que debemos validar?
+        //insertar en una posicion no válida
+        //posiciones negativas
+        //posiciones mayores a la cantidad de elementos
+
+    }
+
 }
