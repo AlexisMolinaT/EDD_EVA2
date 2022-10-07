05d27d3b9ef328e5fdd5bd1ddb7fc04bbcedcce3
AlexisMolinaT <104458053+AlexisMolinaT@users.noreply.github.com>
2022-10-07 09:52:40 -0600

    Add files via upload

---
+++ EVA2_3_LISTA_DOBLE/src/eva2_3_lista_doble/EVA2_3_LISTA_DOBLE.java
@@ -0,0 +1,17 @@
+package eva2_3_lista_doble;
+
+/**
+ *
+ * @author Alexis M
+ */
+public class EVA2_3_LISTA_DOBLE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
