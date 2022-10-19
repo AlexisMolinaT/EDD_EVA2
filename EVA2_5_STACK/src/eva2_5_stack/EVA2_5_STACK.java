eb9f14a232d9834ddf75901d57269bc5ebec4619
AlexisMolinaT <104458053+AlexisMolinaT@users.noreply.github.com>
2022-10-19 09:48:46 -0600

    Add files via upload

---
+++ EVA2_5_STACK/src/eva2_5_stack/EVA2_5_STACK.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_5_stack;
+
+/**
+ *
+ * @author moviles
+ */
+public class EVA2_5_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //add --> push --> agrega un elemento enla cima
+        //peek --> lee el elemento en la cima
+        //pop --> lee y borra el elemento en la cima
+    }
+    
+}
